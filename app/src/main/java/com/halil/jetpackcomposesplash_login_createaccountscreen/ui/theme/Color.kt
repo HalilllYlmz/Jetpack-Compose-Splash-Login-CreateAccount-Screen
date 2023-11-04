9d5d6b3d3c216340641a470581b343f60782bed5
Halil Yılmaz <halilylm1903@gmail.com>
2023-11-04 15:36:58 +0300

    Splash Screen Login Screen Create Account Screen completed

---
+++ app/src/main/java/com/halil/jetpackcomposesplash_login_createaccountscreen/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.halil.jetpackcomposesplash_login_createaccountscreen.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LogoDarkBlue = Color(0xFF191497)
+val LogoBlue = Color(0xFF0e8ccc)
+val LogoRed = Color(0xFFce1728)
+val LogoBlack = Color(0xFF22262e)
+val LogoYellow = Color(0xFFffd400)
+val LogoLightGreen = Color(0xFF33DEB4)
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jetpack Compose Splash-Login-CreateAccount Screen"
+include(":app")
+ 
